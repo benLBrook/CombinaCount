e80caba08557a381e338ed82468688e79de73edc
benLBrook <benealbrook@gmail.com>
2022-07-04 17:50:24 -0500

    add multiply and factorial

--- combifun.c
+++ combifun.c
@@ -13,3 +13,35 @@ void showMainWindow(void)
 	
 }
 
+int multiply(int num, int largeNum[],int arrLength)
+{
+	int carry = 0;
+	int i;
+	for(i = 0; i < arrLength; i++)
+	{
+		int product = largeNum[i] * num + carry;
+		largeNum[i] = product % 10;
+		carry = product / 10;
+	}
+
+	while(carry)
+	{
+		largeNum[arrLength] = carry % 10;
+		carry = carry / 10;
+		arrLength++;
+	}
+	return arrLength;
+}
+
+int * factorial(int n)
+{
+	int largeNum[500];
+	largeNum[0] = 1;
+	int arrLength = 1;
+	int i;
+	for(i = 2; i <= n;i++)
+	{
+		arrLength = multiply(i,largeNum,arrLength);
+	}
+	return largeNum;// Number stored in reverse order.
+}
