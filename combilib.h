5c5416b73cd16c5102e254c02e2589d75304e804
benLBrook <benealbrook@gmail.com>
2022-07-13 22:26:51 -0500

    add divide to library

--- combilib.h
+++ combilib.h
@@ -1,4 +1,5 @@
 void showMainWindow(void);
-int multiply(int num,int largeNum[],int arrLength);//multiply function for large numbers stored in arrays. 
+int multiply(int num,int largeNum[],int arrLength);//multiply function for large numbers stored in arrays.
+int divide(int num,int largeNum[],int arrLength); 
 int * factorial(int n,int *arrLen);
 int * permutation(int n, int r, int *arrLen);
